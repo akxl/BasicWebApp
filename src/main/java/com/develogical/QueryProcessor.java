d98fddca022b8aace5aa7e70cdc4265454d2a9e4
akxl <aaronleong32@gmail.com>
2018-09-27 12:46:46 +0100

    the failing test should pass now

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,9 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("jedi")) {
+            return "The jedi think that they are cool. Also, they fight the sith.";
+        }
         return "";
     }
 }
